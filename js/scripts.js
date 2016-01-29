83aafe4d2709a0ad098148672cee25e5365ac362
Torrence Stratton <student@epicodus.com>
2016-01-29 08:30:05 -0800

    add additional specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
+function Pizza (toppings, size) {
+  this.size = ["Small", "Medium", "Large", "Galaxy-sized"];
+  this.sauces = ["Vegan Cheese", "Carmelized Onions", "Peppers", "Pineapple", "Jalapeno", "Tempeh", "Olives", "Mushrooms", "Onions"];
+  this.order = [];
+}
 
+Word.prototype.pizzaSizer = function() {
+
+}
+
+Word.prototype.toppingSelector = function() {
+  if (this.size )
+}
+
+Word.prototype.costCalc = function() {}
+
+Word.prototype.finalOrder = function() {
+  var order = this.toppingSelector() + this.pizzaSizer() + this.costCalc();
+}
--- spec/specs.js
+++ spec/specs.js
@@ -13,10 +13,18 @@
 describe('Pizza', function() {
   it("will allow a user to choose toppings for the desired pizza", function() {
     var testPizza = new Pizza("Mushroom");
-    expect(testPizza.pizzaOrder()).to.equal("Mushroom");
+    expect(testPizza.toppingSelector()).to.equal("Mushroom");
   });
   it ("will allow a user to choose the size of the pizza", function() {
     var testPizza = new Pizza("Large");
     expect(testPizza.pizzaSizer()).to.equal("Large");
+  });
+  it("will return a pizza based on size and topping selections", function() {
+    var testPizza = new Pizza("Small", "Tempeh", "Tomato Sauce", "Daiya");
+    expect(testPizza.finalOrder()).to.equal(["Small", "Tempeh", "Tomato Sauce", "Daiya"]);
+  });
+  it("will calculate the cost of the pizza based on size and toppings", function() {
+    var testPizza = new Pizza("Small", "Tempeh", "Tomato Sauce", "Daiya");
+    expect(testPizza.costCalc()).to.equal(14);
   });
 });
