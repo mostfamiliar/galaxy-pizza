c31fd88d0df7dab371b704b15ed722121df0bf0e
Torrence Stratton <student@epicodus.com>
2016-01-29 08:15:17 -0800

    add initial specs

--- spec/specs.js
+++ spec/specs.js
@@ -9,3 +9,14 @@
 //       expect(testSplit.wordLengthCalc("Monkey")).to.equal(6);
 //     });
 // });
+
+describe('Pizza', function() {
+  it("will allow a user to choose toppings for the desired pizza", function() {
+    var testPizza = new Pizza("Mushroom");
+    expect(testPizza.pizzaOrder()).to.equal("Mushroom");
+  });
+  it ("will allow a user to choose the size of the pizza", function() {
+    var testPizza = new Pizza("Large");
+    expect(testPizza.pizzaSizer()).to.equal("Large");
+  });
+});
